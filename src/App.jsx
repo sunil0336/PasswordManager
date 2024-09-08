2d1fc9c63f44ff0285ce427f0051fe7d652e71ea
sunil0336 <sunilrathod.0336@gmail.com>
2024-09-09 00:03:09 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <><Navbar/>
       <h1>Vite + React</h1>
       <div className="card">
         <button className='bg-black text-white p-3 rounded-xl' onClick={() => setCount((count) => count + 1)}>
