546713dd5ace17964135d7ca518dc363bcfeb713
sunil0336 <sunilrathod.0336@gmail.com>
2024-09-09 00:17:39 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,8 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <><Navbar/>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button className='bg-black text-white p-3 rounded-xl' onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        
-      </div>
+    <>
+      <Navbar/>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="bg-purple-200 flex justify-between items-center py-3 px-5">
+        <div className="logo font-bold">Password</div>
+        <ul>
+            <li className='flex gap-3 font-bold'>
+                <a href="/">Home</a>
+                <a href="#">About</a>
+                <a href="#">Contact</a>
+            </li>
+        </ul>
+    </nav>
   )
 }
 
