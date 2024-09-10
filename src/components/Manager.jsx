53be9bedefe13a59d761e56d407a055ed20b0233
sunil0336 <sunilrathod.0336@gmail.com>
2024-09-10 21:12:17 +0530

    update

---
+++ src/components/Manager.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Manager = () => {
+  return (
+    <>
+      <div class="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)]"></div>
+      <div className="bg-slate-50 max-w-4xl mycontainer">
+        <h1 className="text-4xl">Password</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa, ipsum.</p>
+        <div className="text-red-600 flex flex-col p-4">
+            <input className="rounded-full" type="text" name="" id="" />
+            <div className="flex">
+            <input type="text" name="" id="" />
+            <input type="text" name="" id="" />
+            </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Manager;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className="bg-purple-200 flex justify-between items-center py-3 px-5">
-        <div className="logo font-bold">Password</div>
-        <ul>
-            <li className='flex gap-3 font-bold'>
-                <a href="/">Home</a>
-                <a href="#">About</a>
-                <a href="#">Contact</a>
-            </li>
-        </ul>
+    <nav className="bg-slate-800 text-white">
+        <div className="mycontainer flex justify-between items-center py-4 px-5">
+          <div className="logo font-bold text-2xl">Password</div>
+          <ul>
+              <li className='flex gap-3'>
+                  <a className='hover:font-bold' href="/">Home</a>
+                  <a className='hover:font-bold' href="#">About</a>
+                  <a className='hover:font-bold' href="#">Contact</a>
+              </li>
+          </ul>
+        </div>
     </nav>
   )
 }
