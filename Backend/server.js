88bda80e81ee7234620094c6c90b0e2569787292
sunil0336 <sunilrathod.0336@gmail.com>
2024-09-18 23:56:44 +0530

    connection with mongodb

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,26 @@
 const express = require('express')
+const dotenv = require('dotenv')
+const {MongoClient} = require('mongodb')
+
+dotenv.config()
+//connection URL
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+//database name
+const dbName = 'passwordm';
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello Sunil!')
+client.connect();
+
+
+app.get('/', async (req, res) => {
+  const db = client.db(dbName);
+  const collection = db.collection('documents');
+  const findResult = await collection.find({}).toArray();
+
+  res.json(findResult)
 })
 
 app.listen(port, () => {
